cfc855b89bf203509c1be9facdd46aff43b9aec9
Viagounet <ismael.rousseau@gmail.com>
2025-07-27 19:50:44 +0000

    initiated custom grpo trainer

--- src/ez_grpo/custom_trainer.py
+++ src/ez_grpo/custom_trainer.py
@@ -1,2 +1,56 @@
+from trl.trainer.grpo_trainer import GRPOTrainer
+from transformers.utils import is_datasets_available, is_peft_available, is_rich_available
+from typing import Optional
+
+import json
+
 class CustomGRPOTrainer(GRPOTrainer):
-    pass
+    def log(self, logs: dict[str, float], start_time: Optional[float] = None) -> None:
+        mode = "train" if self.model.training else "eval"
+        metrics = {key: sum(val) / len(val) for key, val in self._metrics[mode].items()}  # average the metrics
+
+        # This method can be called both in training and evaluation. When called in evaluation, the keys in `logs`
+        # start with "eval_". We need to add the prefix "eval_" to the keys in `metrics` to match the format.
+        if mode == "eval":
+            metrics = {f"eval_{key}": val for key, val in metrics.items()}
+
+        logs = {**logs, **metrics}
+        super().log(logs, start_time)
+        self._metrics[mode].clear()
+
+        if self.accelerator.is_main_process and self.log_completions:
+            if is_rich_available():
+                print_prompt_completions_sample(
+                    self._textual_logs["prompt"],
+                    self._textual_logs["completion"],
+                    self._textual_logs["rewards"],
+                    self._textual_logs["advantages"],
+                    self.state.global_step,
+                    self.num_completions_to_print,
+                )
+
+            if self.args.report_to and "wandb" in self.args.report_to and wandb.run is not None:
+                import pandas as pd
+
+                table = {
+                    "step": [str(self.state.global_step)] * len(self._textual_logs["prompt"]),
+                    "prompt": self._textual_logs["prompt"],
+                    "completion": self._textual_logs["completion"],
+                    **self._textual_logs["rewards"],
+                    "advantage": self._textual_logs["advantages"],
+                }
+                df = pd.DataFrame(table)
+                if self.wandb_log_unique_prompts:
+                    df = df.drop_duplicates(subset=["prompt"])
+                wandb.log({"completions": wandb.Table(dataframe=df)})
+
+            if self.args.report_to and "tensorboard" in self.args.report_to: # Note: "custom" would be cleaner, but need to change Trainer as well
+                table = {
+                    "step": [str(self.state.global_step)] * len(self._textual_logs["prompt"]),
+                    "prompt": self._textual_logs["prompt"],
+                    "completion": self._textual_logs["completion"],
+                    **self._textual_logs["rewards"],
+                    "advantage": self._textual_logs["advantages"],
+                }
+                with open(self.args.output_dir, "r", encoding="utf-8") as f:
+                    json.dump(table, f, ensure_ascii=False, indent=4)
--- src/ez_grpo/trainer.py
+++ src/ez_grpo/trainer.py
@@ -1,6 +1,7 @@
 # train_grpo.py
 from datasets import load_dataset
-from trl import GRPOConfig, GRPOTrainer
+from trl import GRPOConfig
+from custom_trainer import CustomGRPOTrainer
 
 dataset = load_dataset("trl-lib/tldr", split="train")
 
@@ -8,8 +9,8 @@ dataset = load_dataset("trl-lib/tldr", split="train")
 def reward_len(completions, **kwargs):
     return [-abs(20 - len(completion)) for completion in completions]
 
-training_args = GRPOConfig(output_dir="Qwen2-0.5B-GRPO")
-trainer = GRPOTrainer(
+training_args = GRPOConfig(output_dir="Qwen2-0.5B-GRPO", fp16=True, report_to="tensorboard", logging_strategy="steps", logging_steps=1)
+trainer = CustomGRPOTrainer(
     model="Qwen/Qwen2-0.5B-Instruct",
     reward_funcs=reward_len,
     args=training_args,
